4c571f0163c52b087d277940a2dfb586bca0d200
James Snell <vulpes3.1415@yahoo.com>
2023-12-07 16:58:37 -0700

    Completed first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+// Assign functions to a variable.
+// Store functions in a data structure.
+// Write functions that return other functions.
+// Pass a function to another function.
+// Call a function returned by another function.
+
+
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2);
+};
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2);
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = (multiplier) => {
+    return (fare) => {
+        return multiplier * fare;
+    };
+};
+
+
+const fareDoubler = function (fare) {
+  const doubleFare = createFareMultiplier(2)
+    return doubleFare(fare);
+};
+
+
+const fareTripler = function (fare) {
+    const fareTripler = createFareMultiplier(3)
+      return fareTripler(fare);
+  };
+
+
+const selectDifferentDrivers = (driverArray, callBack) => {
+    return callBack(driverArray);
+};
